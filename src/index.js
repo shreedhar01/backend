566e8c247831ba850bd4ede3223757ee9ae48c76
shreedhar01 <shreedharkalamagar@gmail.com>
2025-01-28 17:21:28 +0000

    app.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
-import connectDB from './db/index.js'
-import dotenv from "dotenv"
-dotenv.config({path:"./.env"})
-
-
-
-
+const listening = process.env.PORT || 8000;
 connectDB()
+  .then(() => {
+    app.listen(listening, () => {
+      console.log(`App is watching at : ${listening}`);
+    });
+    app.on("error",(error)=>{
+        console.log(`Error on app: ${error}`)
+    })
+  })
+  .catch((err) => {
+    console.log(`MongoDB Connection failed at : ${err}`);
+  });
