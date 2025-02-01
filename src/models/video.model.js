7ecd6f4265079ae365b446f5ff553232acca930b
shreedhar001 <kano78bei007@kec.edu.np>
2025-02-01 07:55:52 +0000

    user and video models are added

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile:{
+        type: String,   //from cloudianary
+        required: true
+    },
+    Thumbnails:{
+        type: String,
+        required: true    
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    duration:{
+        type: Number,
+        required: true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPublish:{
+        type: Boolean,
+        default: true
+    }
+},{timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
