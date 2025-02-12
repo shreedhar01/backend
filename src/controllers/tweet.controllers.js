7971288b62802629d5b252fe0d32a59f09e8d5c3
shreedhar001 <kano78bei007@kec.edu.np>
2025-02-12 15:53:38 +0000

    add tweet retrieval, update, and delete functionality with appropriate error handling and  all are check throught postman

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -2,11 +2,12 @@ import { asyncHandler } from "../utils/asyncHandeler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Tweet } from "../models/tweet.model.js";
+import mongoose from "mongoose";
 
 const createTweet = asyncHandler(async (req, res) => {
     const userId = req.user?._id
-    const {content} = req.body
-    if(!content?.trim()){
+    const { content } = req.body
+    if (!content?.trim()) {
         throw new ApiError(400, "No content to add")
     }
 
@@ -14,7 +15,7 @@ const createTweet = asyncHandler(async (req, res) => {
         owner: userId,
         content: content.trim()
     })
-    if(!tweet){
+    if (!tweet) {
         throw new ApiError(500, "Failed to create tweet")
     }
 
@@ -23,6 +24,100 @@ const createTweet = asyncHandler(async (req, res) => {
     )
 })
 
+const getUserTweet = asyncHandler(async (req, res) => {
+    const userId = req.user?._id
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: { owner: new mongoose.Types.ObjectId(userId) }
+        },
+        {
+            $project: {
+                _id: 1,
+                createdAt: 1,
+                content: 1
+            }
+        },
+        {
+            $sort: { createdAt: -1 }
+        }
+    ])
+    if (!tweets) {
+        throw new ApiError(400, "Either owner and tweet id dont match or tweet dont exist")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            tweets,
+            "tweet get successfully"
+        )
+    )
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    const { content } = req.body
+    const { tweetId } = req.params
+    const userId = req.user?._id
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Sorry Content is required")
+    }
+
+
+    const updatedTweet = await Tweet.findOneAndUpdate(
+        {
+            _id: tweetId,
+            owner: userId
+        },
+        {
+            $set: {
+                content: content.trim()
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+
+
+    if (!updatedTweet) {
+        throw new ApiError(400, "Tweet not found or you don't have permission to update")
+    }
+    return res.status(200).json(
+        new ApiResponse(200, updatedTweet, "Tweet updated successfully")
+    )
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+    const userId = req.user?._id
+
+    if (!tweetId?.trim()) {
+        throw new ApiError(400, "sorry tweetid not found")
+    }
+
+    const deletedTweet = await Tweet.deleteOne(
+        {
+            _id: tweetId,
+            owner: userId
+        }
+    )
+
+    if (!deletedTweet.acknowledged) {
+        throw new ApiError(400, "Tweet not found or you don't have permission to delete")
+    }
+    return res.status(200).json(
+        new ApiResponse(200, {}, "Tweet deleted successfully")
+    )
+})
+
+
+
 export {
     createTweet,
+    getUserTweet,
+    updateTweet,
+    deleteTweet
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import {
-    createTweet
+    createTweet,
+    getUserTweet,
+    updateTweet,
+    deleteTweet
 } from "../controllers/tweet.controllers.js";
 import { limiter } from "../middlewares/rateLimiter.middlewares.js";
 
 const router = Router()
 
 router.use(verifyJWT)
-router.route('/').post(limiter, createTweet)
+router.route('/').post(limiter, createTweet).get(getUserTweet)
+router.route('/:tweetId').delete(deleteTweet).patch(updateTweet)
 
 export default router
