24a1c1bb36a3f833accd59d58e532f3b9436badd
shreedhar001 <kano78bei007@kec.edu.np>
2025-02-20 18:03:24 +0000

    add toggleVideoLike functionality to manage video likes and dislikes

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,40 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandeler.js";
+import { Like } from "../models/likes.model.js";
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+
+const toggleVideoLike = asyncHandler(async (req, res)=>{
+    const {likeId} = req.query
+    const userId = req.user?._id
+    const {videoId} = req.params
+
+    const isVideoExist = await Video.findById(videoId)
+    if(!isVideoExist){
+        throw new ApiError(403,"video not found")
+    }
+
+    const likeExist = await Like.findOneAndDelete({
+        _id: likeId,
+        likedby: userId
+    })
+    if(likeExist){
+        return res.status(200).json(
+            new ApiResponse(200, {}, "video is disliked")
+        )
+    }else{
+        const likeCreated = await Like.create({
+            likedby:userId,
+            video: videoId
+        })
+
+        return res.status(200).json(
+            new ApiResponse(200, likeCreated, "video is liked")
+        )
+    }
+})
+
+export {
+    toggleVideoLike,
+}
